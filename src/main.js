6805c1d1c030511783ae2a966dc8a17ed7db38a5
Dziqha <lulussnbp01@gmail.com>
2024-02-12 09:24:11 +0700

    fist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
+// main.js
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).mount("#app");
+import Products from "./components/Products";
+import Checkout from "./components/Checkout.vue";
+
+library.add(faShoppingCart, faDollarSign);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Products
+    },
+    {
+      path: "/checkout",
+      component: Checkout
+    }
+  ]
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
